304165d82f6b7d57776531ea2feb368c4529e89e
Kirit Sælensminde <kirit@felspar.com>
2025-08-28 17:00:40 +0700

    Make these functions constexpr

--- array.hpp
+++ array.hpp
@@ -1,6 +1,6 @@
 /**
 {{field created 2025-08-20T03:42:31.726Z}}
-{{field lastUpdate 2025-08-23T06:16:27.443Z}}
+{{field lastUpdate 2025-08-28T10:00:06.159Z}}
 {{field displayName Tiny Classifiers/array.hpp}}
  */
 #pragma once
@@ -13,7 +13,7 @@
 
 /// ## `array_of` -- Create an array of `N` values
 template<std::size_t N, std::invocable<std::size_t> V>
-inline auto array_of(V &&v) {
+constexpr inline auto array_of(V &&v) {
     return [&]<std::size_t... Indices>(
                    std::integer_sequence<std::size_t, Indices...>) {
         return std::array{v(Indices)...};
@@ -21,6 +21,6 @@ inline auto array_of(V &&v) {
 }
 
 template<std::size_t N, std::copyable V>
-inline auto array_of(V const &v) {
+constexpr inline auto array_of(V const &v) {
     return array_of<N>([&](std::size_t) { return v; });
 }
--- functional.hpp
+++ functional.hpp
@@ -1,6 +1,6 @@
 /**
 {{field created 2025-08-21T06:54:17.304Z}}
-{{field lastUpdate 2025-08-23T04:02:18.400Z}}
+{{field lastUpdate 2025-08-28T10:00:06.159Z}}
 {{field displayName Tiny Classifiers/functional.hpp}}
  */
 #pragma once
@@ -10,7 +10,7 @@
 
 
 template<typename Lambda>
-inline auto by_index(
+constexpr inline auto by_index(
         std::size_t const start_index,
         std::size_t const max_index,
         Lambda &&lambda) {
@@ -19,6 +19,6 @@ inline auto by_index(
     }
 }
 template<typename Lambda>
-inline auto by_index(std::size_t const max_index, Lambda &&lambda) {
+constexpr inline auto by_index(std::size_t const max_index, Lambda &&lambda) {
     return by_index({}, max_index, std::forward<Lambda>(lambda));
 }
